27cdce2a7223de157425e1b7c6983693262bf12d
saqlain-git <alysaqlain0072@gmail.com>
2025-10-21 22:32:20 +0500

    LEC 2

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
-change
+# Data store. -> sigle lie comment.
+
+a = 5 # -> int
+
+b = 5.1 # -> float
+
+c = "ammar" # -> String .
+
+d = True  # -> bool 
+
+e = 5j # -> complex
+
+# Type Function.  type(Var) -> type of var
+
+# print("Type a: ", type(a))  
+# print("Type b: ", type(b))  
+# print("Type c: ", type(c))  
+# print("Type d: ", type(d))  
+# print("Type e: ", type(e))  
+
+# Artimatic Operators.
+# + -> add
+# - -> sub 
+# * -> mul
+# / -> div
+#  // -> floor div
+#  % > mod reminder retrun. 
 
 
+# MOD:
+"""
+Mod retruns the remider of a division perfomed.
+
+5/2 -> 1 , 2.5
+1 -> will be retruned by mod.
+
+"""
+
+# some extra things.
+#  if addition is perfomed btw 2 str then they are cocetenated. and if multuplyed its repeted.
+f = "HI"
+g = "BY"
+# print(f * g)
+
+
+al = True  # 1
+af = False # 0
+
+print(al + af)  # 0 + 1 = 1
